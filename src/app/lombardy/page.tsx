9715b145deeff3eb4fd2490508ebc3b5aed4fef0
ramya72002 <nsriramya7@gmail.com>
2024-09-11 16:50:52 +0530

    code restructured

--- src/app/lombardy/page.tsx
+++ src/app/lombardy/page.tsx
@@ -1,9 +1,9 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react/no-unescaped-entities */
 'use client'
 import React, { useState } from 'react';
 import axios from 'axios';
 import './lombardy.scss';
+import Link from 'next/link';
 
 const ChessRegistration = () => {
   const [formData, setFormData] = useState({
@@ -14,7 +14,7 @@ const ChessRegistration = () => {
     child_grade: '',
     email: '',
     phone: '',
-    acceptTerms: false, // Added for checkbox
+    acceptTerms: false,
     RequestFinancialAssistance: false,
     SchoolName: "Lombardy Elementary School",
   });
@@ -25,7 +25,6 @@ const ChessRegistration = () => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value, type } = e.target;
   
-    // Explicitly handle the type of the target
     if (type === 'checkbox') {
       const checked = (e.target as HTMLInputElement).checked;
       setFormData({
@@ -39,7 +38,6 @@ const ChessRegistration = () => {
       });
     }
   };
-  
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -80,11 +78,8 @@ const ChessRegistration = () => {
     formData.RequestFinancialAssistance = true;
 
     try {
-      // First API call
       const response1 = await axios.post('https://backend-chess-tau.vercel.app/send-email-form-lombardy', formData);
       if (response1.status === 200) {
-        
-        // Second API call
         const response2 = await axios.post('https://backend-chess-tau.vercel.app/submit_form', formData);
         if (response2.status === 201) {
           setShowThankYou(true);
@@ -108,20 +103,16 @@ const ChessRegistration = () => {
         </div>
       )}
 
-{showThankYou && (
-  <div className="thank-you-overlay">
-     <p className="thank-you-message">
-      <span>Thank you!</span> We’re excited about your interest in enrolling your child in our chess program. We understand that you are seeking financial assistance, and we appreciate your inquiry. Please allow us some time to review your request, and we will get back to you shortly.
-      <br />
-      <br />
-      Rest assured, we are fully committed to supporting your child's development and helping them explore the many benefits that chess offers during these formative years. We look forward to providing a fun, enriching experience that will help nurture their skills and growth.
-    </p>
-  </div>
-)}
-
-
-
-
+      {showThankYou && (
+        <div className="thank-you-overlay">
+          <p className="thank-you-message">
+            <span>Thank you!</span> We’re excited about your interest in enrolling your child in our chess program. We understand that you are seeking financial assistance, and we appreciate your inquiry. Please allow us some time to review your request, and we will get back to you shortly.
+            <br />
+            <br />
+            Rest assured, we are fully committed to supporting your child's development and helping them explore the many benefits that chess offers during these formative years. We look forward to providing a fun, enriching experience that will help nurture their skills and growth.
+          </p>
+        </div>
+      )}
 
       {!loading && !showThankYou && (
         <>
@@ -151,99 +142,94 @@ const ChessRegistration = () => {
 
           <div className="training-info">
             <p><strong>10 Week Training [K-5 Students]</strong></p>
-            <p>26 Oct 2024 to 19 Oct 2024</p>
-            <p>[classes on 9/26; 10/10, 10/17, 10/31, 11/14, 11/21, 11/28 ; 12/5 and 12/12 .No class on 10/3 and 10/24 Oct-2024]</p>
-            <p>3:30 PM - 4:30 PM</p>
+            <p> Program Date: 26 Sep 2024 to 12 Dec 2024</p>
+            <p>[classes on 9/26; 10/10, 10/17, 10/31, 11/14, 11/21, 11/28 ; 12/5 and 12/12 .No class on 10/3 and 10/24]</p>
+            <p>Time: 3:30 PM - 4:30 PM</p>
           </div>
 
           <form className="registration-form" onSubmit={handleSubmit}>
-  <div className="input-group">
-    <label>Parent's Name</label>
-    <div className="input-row">
-      <input 
-        type="text" 
-        name="parent_first_name" 
-        placeholder="First" 
-        value={formData.parent_first_name} 
-        onChange={handleChange}
-        // required
-      />
-      <input 
-        type="text" 
-        name="parent_last_name" 
-        placeholder="Last" 
-        value={formData.parent_last_name} 
-        onChange={handleChange}
-        // required
-      />
-    </div>
-  </div>
+            <div className="input-group">
+              <label>Parent's Name</label>
+              <div className="input-row">
+                <input 
+                  type="text" 
+                  name="parent_first_name" 
+                  placeholder="First" 
+                  value={formData.parent_first_name} 
+                  onChange={handleChange}
+                />
+                <input 
+                  type="text" 
+                  name="parent_last_name" 
+                  placeholder="Last" 
+                  value={formData.parent_last_name} 
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
 
-  <div className="input-group">
-    <label>Child's Name</label>
-    <div className="input-row">
-      <input 
-        type="text" 
-        name="child_first_name" 
-        placeholder="First" 
-        value={formData.child_first_name} 
-        onChange={handleChange}
-        // required
-      />
-      <input 
-        type="text" 
-        name="child_last_name" 
-        placeholder="Last" 
-        value={formData.child_last_name} 
-        onChange={handleChange}
-        // required
-      />
-    </div>
-  </div>
+            <div className="input-group">
+              <label>Child's Name</label>
+              <div className="input-row">
+                <input 
+                  type="text" 
+                  name="child_first_name" 
+                  placeholder="First" 
+                  value={formData.child_first_name} 
+                  onChange={handleChange}
+                />
+                <input 
+                  type="text" 
+                  name="child_last_name" 
+                  placeholder="Last" 
+                  value={formData.child_last_name} 
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
 
-  <div className="input-group">
-    <label>Child's Grade</label>
-    <select 
-      name="child_grade" 
-      value={formData.child_grade} 
-      onChange={handleChange}
-      required
-    >
-      <option value="">Dropdown</option>
-      <option value="K">K</option>
-      <option value="1">1st Grade</option>
-      <option value="2">2nd Grade</option>
-      <option value="3">3rd Grade</option>
-      <option value="4">4th Grade</option>
-      <option value="5">5th Grade</option>
-    </select>
-  </div>
+            <div className="input-group">
+              <label>Child's Grade</label>
+              <select 
+                name="child_grade" 
+                value={formData.child_grade} 
+                onChange={handleChange}
+                required
+              >
+                <option value="">Dropdown</option>
+                <option value="K">K</option>
+                <option value="1">1st Grade</option>
+                <option value="2">2nd Grade</option>
+                <option value="3">3rd Grade</option>
+                <option value="4">4th Grade</option>
+                <option value="5">5th Grade</option>
+              </select>
+            </div>
 
-  <div className="input-group">
-    <label>Email <span className="required">*</span></label>
-    <input 
-      type="email" 
-      name="email" 
-      placeholder="Enter Email" 
-      value={formData.email} 
-      onChange={handleChange}
-      required
-    />
-  </div>
+            <div className="input-group">
+              <label>Email <span className="required">*</span></label>
+              <input 
+                type="email" 
+                name="email" 
+                placeholder="Enter Email" 
+                value={formData.email} 
+                onChange={handleChange}
+                required
+              />
+            </div>
 
-  <div className="input-group">
-    <label>Phone <span className="required">*</span></label>
-    <input 
-      type="tel" 
-      name="phone" 
-      placeholder="Enter Phone Number" 
-      value={formData.phone} 
-      onChange={handleChange}
-      required
-    />
-  </div>
+            <div className="input-group">
+              <label>Phone <span className="required">*</span></label>
+              <input 
+                type="tel" 
+                name="phone" 
+                placeholder="Enter Phone Number" 
+                value={formData.phone} 
+                onChange={handleChange}
+                required
+              />
+            </div>
 
-  
             <div className="training-info">
               <p><strong>10 Week Program $150.00</strong></p>
             </div>
@@ -256,11 +242,10 @@ const ChessRegistration = () => {
                 onChange={handleChange} 
               />
               <label htmlFor="terms">
-                I accept the <a href="https://chess-club-frontend.vercel.app/terms-and-conditions" target="_blank" rel="noopener noreferrer">terms and conditions</a>
+                I accept the <Link href="/terms-and-conditions">terms and conditions</Link>
               </label>
             </div>
 
-            {/* Make Payment button */}
             <div className="button-group">
               <button type="submit" className="payment-button" disabled={loading}>Make Payment</button>
             </div>
