5426ef821a13824c40a73687accad830753b5c40
ramya72002 <nsriramya7@gmail.com>
2024-09-23 16:29:43 +0530

    links added

--- src/components/CheckoutPage.tsx
+++ src/components/CheckoutPage.tsx
@@ -121,7 +121,7 @@ const CheckoutPage: React.FC<CheckoutPageProps> = ({ amount, formData }) => {
           className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
           role="status"
         >
-          <span className="sr-only">Loading...</span>
+          <span className="sr-only">Loading....</span>
         </div>
       </div>
     );
