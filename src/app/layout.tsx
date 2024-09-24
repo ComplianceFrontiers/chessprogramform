f7ff1d5ddaf24ab39dc5b7c42b73285f7935e5f9
ramya72002 <nsriramya7@gmail.com>
2024-09-24 14:50:30 +0530

    level1 vedio added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Chess Champs",
-  description: "Chess Champs",
+  description: "Chess Champs", 
 };
 
 export default function RootLayout({
