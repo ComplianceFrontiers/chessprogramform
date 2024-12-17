96c5004c85689c7f1478d6b210f29d4ad6aca344
ramya72002 <nsriramya7@gmail.com>
2024-12-17 14:53:42 +0530

    master list

--- src/app/SendEmail.tsx
+++ src/app/SendEmail.tsx
@@ -30,7 +30,7 @@ const SendEmail: React.FC<SendEmailProps> = ({ selectedRecords, onBack }) => {
   const emailBody = `
   <div style="font-family: Arial, sans-serif; color: #333; padding: 20px; text-align: center;display: block;">
     <div style="text-align: center;">
-      <img src="/images/image.png" alt="Chess Champs Banner" style="max-width: 100%; height: auto;">
+      <img src="https://res.cloudinary.com/dtgje24ez/image/upload/v1734427370/zianrqdokutklmiibvg0.png" alt="Chess Champs Banner" style="max-width: 100%; height: auto;">
     </div>
     <h1 style="color: #000; text-align: left; font-size: 14px; font-weight: bold; margin-bottom: 20px;margin-top: 20px;">
       Now enrolling for the 2025 Winter/Spring Session of Chess Champs, beginning January 21, 2025, and concluding with a showcase on Sunday, April 27, 2025, at 3 PM.
