396c526cf55b4b3fa96e0c29a3a26062e813e832
ramya72002 <nsriramya7@gmail.com>
2024-12-17 14:44:24 +0530

    master list

--- src/app/SendEmail.tsx
+++ src/app/SendEmail.tsx
@@ -40,8 +40,8 @@ const SendEmail: React.FC<SendEmailProps> = ({ selectedRecords, onBack }) => {
     </p>
 
     <!-- Programs -->
-    <h2 style="text-align: left; color: #ff0080; margin-bottom: 10px;">Programs:</h2>
-   <ul style="text-align: left; padding-left: 20px; font-size: 14px; line-height: 1.6; list-style-type: disc;">
+    <h2 style="text-align: left; color: #f53db8; margin-bottom: 10px;">Programs:</h2>
+   <ul style="text-align: left; padding-left: 50px; font-size: 14px; line-height: 1.6; list-style-type: disc;">
   <li><strong>Creative Movement</strong> (Ages 4 - 6)</li>
   <li><strong>Ballet 1</strong> (Ages 7 - 9)</li>
   <li><strong>Ballet 2</strong> (Ages 10 - 12)</li>
@@ -54,19 +54,19 @@ const SendEmail: React.FC<SendEmailProps> = ({ selectedRecords, onBack }) => {
    Check out our schedule & class descriptions below. Join us and discover the joy of dance in a supportive and creative environment. We can’t wait to see you there!
     </p>
 
-    <h2 style="text-align: left; color: #ff0080; margin-bottom: 10px;">Cost:</h2>
-    <ul style="text-align: left;list-style-type: disc; padding-left: 20px; font-size: 14px; line-height: 1.6;">
+    <h2 style="text-align: left; color: #f53db8; margin-bottom: 10px;">Cost:</h2>
+    <ul style="text-align: left;list-style-type: disc; padding-left: 50px; font-size: 14px; line-height: 1.6;">
       <li>Once weekly programs: $150/Member, $200/Non-Member</li>
       <li>Twice weekly programs: $250/Member, $300/Non-Member</li>
     </ul>
 
     <div style="text-align: center; margin-top: 30px;">
-      <a href="${apiLink}" style="display: inline-block; padding: 12px 25px; background-color: #ff0080; color: white; text-align: center; border-radius: 5px; text-decoration: none; font-size: 16px;">
+      <a href="${apiLink}" style="display: inline-block; padding: 12px 25px; background-color: #f53db8; color: white; text-align: center; border-radius: 5px; text-decoration: none; font-size: 16px;">
         REGISTER HERE
       </a>
     </div>
 
-    <h2 style="text-align: left; color: #ff0080; margin-top: 20px; margin-bottom: 10px;">SHOWCASE:</h2>
+    <h2 style="text-align: left; color: #f53db8; margin-top: 20px; margin-bottom: 10px;">SHOWCASE:</h2>
     <p style="font-size: 14px; line-height: 1.6; margin-top: 20px; margin-bottom: 10px; text-align: left; max-width: 800px; margin-left: auto; margin-right: auto;">
 Interested in getting a sneak peek at our program? Come check out the Showcase for the current session! It will be held on <strong>Sunday, December 22 at 3 PM</strong> in the <strong>Auditorium</strong>.
 </p>
@@ -74,31 +74,68 @@ Interested in getting a sneak peek at our program? Come check out the Showcase f
 
     <!-- Schedule -->
     <h2 style="text-align: left; color: #ffffff; padding:10px; margin-top: 20px; margin-bottom: 10px; background-color: #000000;">Schedule:</h2>
-   <ul style="text-align: left; padding-left: 20px; font-size: 14px; line-height: 1.6; list-style: disc;">
- <b style="color: #ff0080; font-size: 18px;">Creative Movement (Ages 4 - 6)</b>
-
-  <li><strong><a href="#" style="color: blue; text-decoration: underline;">K/1st Graders on Tuesdays</a></strong>, Jan 21-Apr 22, 4:45-5:30 PM with Ms. Brenna in Studio 2</li>
-  <li><strong><a href="#" style="color: blue; text-decoration: underline;">Pre-K on Thursdays</a></strong>: Jan 23-Apr 24, 4:45-5:30 PM with Ms. Sam in Studio 2</li>
-  <li><strong><a href="#" style="color: blue; text-decoration: underline;">Combined Ages 4-6 on Saturdays</a></strong>: Jan 25-Apr 26, 9:15-10 AM with Ms. Sarah in the Mercaz</li>
-
-  <b style="color: #ff0080;font-size: 18px;">Ballet 1 (Ages 7 - 9)</b>
-  <li><strong><a href="#" style="color: blue; text-decoration: underline;">Tuesdays</a></strong>, Jan 21-Apr 22, 5:45-6:45 PM with Ms. Brenna in the Mercaz</li>
-  <li><strong><a href="#" style="color: blue; text-decoration: underline;">Saturdays</a></strong>, Jan 25-Apr 26: 11:15 AM-12:15 PM with Ms. Sarah in the Mercaz</li>
-
-  <b style="color: #ff0080;font-size: 18px;">Ballet 2 (Ages 10 - 12)</b>
-  <li><strong><a href="#" style="color: blue; text-decoration: underline;">Thursdays & Sundays</a></strong>, Jan 23-Apr 20: 6:45-8 PM Thursdays & 11 AM-12:15 PM Sundays with Mr. Leo in the Mercaz</li>
-
-  <b style="color: #ff0080;font-size: 18px;">Jazz (Ages 7 - 12)</b>
-  <li><strong><a href="#" style="color: blue; text-decoration: underline;">Tuesdays</a></strong>, Jan 21-Apr 22: 7-8 PM with Ms. Brenna in the Mercaz</li>
-  <li><strong><a href="#" style="color: blue; text-decoration: underline;">Saturdays</a></strong>, Jan 25-Apr 26: 10-11 AM with Ms. Sarah in the Mercaz</li>
-
-  <b style="color: #ff0080;font-size: 18px;">Adult Ballet (Ages 18+)</b>
-  <li><strong><a href="#" style="color: blue; text-decoration: underline;">Beginner on Wednesdays</a></strong>, Jan 22-Apr 23, 7-8 PM with Mr. Leo in Studio 2</li>
-  <li><strong><a href="#" style="color: blue; text-decoration: underline;">Intermediate on Sundays</a></strong>, Jan 26-Apr 20, 8:45-9:45 AM with Mr. Leo in the Mercaz</li>
+   <ul style="text-align: left; padding-left: 0px; font-size: 14px; line-height: 1.6; list-style: disc;">
+ <b style="color: #f53db8; font-size: 16px;">Creative Movement (Ages 4 - 6)</b>
+<ul style="padding-left: 30px; list-style-type: disc;">
+  <li style="padding-left: 10px;">
+    <strong><a href="#" style="color: blue; text-decoration: underline;">K/1st Graders on Tuesdays</a></strong>, 
+    Jan 21-Apr 22, 4:45-5:30 PM with Ms. Brenna in Studio 2
+  </li>
+  <li style="padding-left: 10px;">
+    <strong><a href="#" style="color: blue; text-decoration: underline;">Pre-K on Thursdays</a></strong>: 
+    Jan 23-Apr 24, 4:45-5:30 PM with Ms. Sam in Studio 2
+  </li>
+  <li style="padding-left: 10px;">
+    <strong><a href="#" style="color: blue; text-decoration: underline;">Combined Ages 4-6 on Saturdays</a></strong>: 
+    Jan 25-Apr 26, 9:15-10 AM with Ms. Sarah in the Mercaz
+  </li>
 </ul>
 
+<b style="color: #f53db8; font-size: 16px;">Ballet 1 (Ages 7 - 9)</b>
+<ul style="padding-left: 30px; list-style-type: disc;">
+  <li style="padding-left: 10px;">
+    <strong><a href="#" style="color: blue; text-decoration: underline;">Tuesdays</a></strong>, 
+    Jan 21-Apr 22, 5:45-6:45 PM with Ms. Brenna in the Mercaz
+  </li>
+  <li style="padding-left: 10px;">
+    <strong><a href="#" style="color: blue; text-decoration: underline;">Saturdays</a></strong>, 
+    Jan 25-Apr 26: 11:15 AM-12:15 PM with Ms. Sarah in the Mercaz
+  </li>
+</ul>
+
+<b style="color: #f53db8; font-size: 16px;">Ballet 2 (Ages 10 - 12)</b>
+<ul style="padding-left: 30px; list-style-type: disc;">
+  <li style="padding-left: 10px;">
+    <strong><a href="#" style="color: blue; text-decoration: underline;">Thursdays & Sundays</a></strong>, 
+    Jan 23-Apr 20: 6:45-8 PM Thursdays & 11 AM-12:15 PM Sundays with Mr. Leo in the Mercaz
+  </li>
+</ul>
+
+<b style="color: #f53db8; font-size: 16px;">Jazz (Ages 7 - 12)</b>
+<ul style="padding-left: 30px; list-style-type: disc;">
+  <li style="padding-left: 10px;">
+    <strong><a href="#" style="color: blue; text-decoration: underline;">Tuesdays</a></strong>, 
+    Jan 21-Apr 22: 7-8 PM with Ms. Brenna in the Mercaz
+  </li>
+  <li style="padding-left: 10px;">
+    <strong><a href="#" style="color: blue; text-decoration: underline;">Saturdays</a></strong>, 
+    Jan 25-Apr 26: 10-11 AM with Ms. Sarah in the Mercaz
+  </li>
+</ul>
+
+<b style="color: #f53db8; font-size: 16px;">Adult Ballet (Ages 18+)</b>
+<ul style="padding-left: 30px; list-style-type: disc;">
+  <li style="padding-left: 10px;">
+    <strong><a href="#" style="color: blue; text-decoration: underline;">Beginner on Wednesdays</a></strong>, 
+    Jan 22-Apr 23, 7-8 PM with Mr. Leo in Studio 2
+  </li>
+  <li style="padding-left: 10px;">
+    <strong><a href="#" style="color: blue; text-decoration: underline;">Intermediate on Sundays</a></strong>, 
+    Jan 26-Apr 20, 8:45-9:45 AM with Mr. Leo in the Mercaz
+  </li>
+</ul>
 <div style="text-align: center; margin-top: 30px;">
-      <a href="${apiLink}" style="display: inline-block; padding: 12px 25px; background-color: #ff0080; color: white; text-align: center; border-radius: 5px; text-decoration: none; font-size: 16px;">
+      <a href="${apiLink}" style="display: inline-block; padding: 12px 25px; background-color: #f53db8; color: white; text-align: center; border-radius: 5px; text-decoration: none; font-size: 16px;">
         REGISTER HERE
       </a>
     </div>
@@ -142,22 +179,22 @@ Interested in getting a sneak peek at our program? Come check out the Showcase f
     <div style="margin-top: 20px;">
         <div style="background-color: #343a40; color: #fff; padding: 20px; border-radius: 5px 5px 0 0;">
             <h1 style="margin-bottom: 10px;">About the J</h1>
-            <p style="margin-bottom: 10px;">The Siegel JCC is a non-profit Jewish community center where people of all faiths and backgrounds come together to learn, share, play, and celebrate. Our 32-acre campus houses an Early Childhood Center, Youth & Teen Programs, and Summer Camp, as well as a state-of-the-art Fitness Center, Indoor & Outdoor Pools, Sports, and Community Events.</p>
+            <p style="margin-bottom: 10px; font-size:10px">The Siegel JCC is a non-profit Jewish community center where people of all faiths and backgrounds come together to learn, share, play, and celebrate. Our 32-acre campus houses an Early Childhood Center, Youth & Teen Programs, and Summer Camp, as well as a state-of-the-art Fitness Center, Indoor & Outdoor Pools, Sports, and Community Events.</p>
         </div>
 
         <div style="background-color: #007BFF; color: #fff; padding: 20px; border-radius: 0 0 5px 5px; text-align: center;">
-            <div style="margin-bottom: 15px;">
+            <div style="margin-bottom: 15px; font-size:10px">
                 <a href="#" target="_blank" style={{ margin: '0 10px' }}>
   <FontAwesomeIcon icon={faFacebook} style={{ fontSize: '20px', color: 'white' }} />
 </a>
 <a href="#" target="_blank" style={{ margin: '0 10px' }}>
-  <FontAwesomeIcon icon={faYoutube} style={{ fontSize: '20px', color: 'white' }} />
+  <FontAwesomeIcon icon={faYoutube} style={{ fontSize: '10px', color: 'white' }} />
 </a>
 <a href="#" target="_blank" style={{ margin: '0 10px' }}>
-  <FontAwesomeIcon icon={faTwitter} style={{ fontSize: '20px', color: 'white' }} />
+  <FontAwesomeIcon icon={faTwitter} style={{ fontSize: '10px', color: 'white' }} />
 </a>
 <a href="#" target="_blank" style={{ margin: '0 10px' }}>
-  <FontAwesomeIcon icon={faInstagram} style={{ fontSize: '20px', color: 'white' }} />
+  <FontAwesomeIcon icon={faInstagram} style={{ fontSize: '10px', color: 'white' }} />
 </a>
 <a href="#" target="_blank" style={{ margin: '0 10px' }}>
   <FontAwesomeIcon icon={faLinkedin} style={{ fontSize: '20px', color: 'white' }} />
@@ -166,9 +203,9 @@ Interested in getting a sneak peek at our program? Come check out the Showcase f
 
             <div>
                 <p style="margin: 0;">SIEGEL JCC</p>
-               <p style="margin: 0;">
+               <p style="margin: 0;font-size:10px">
   <a href="https://www.google.com/maps?q=101+Garden+of+Eden+Road,+Wilmington,+DE+19803" target="_blank" style="text-decoration: underline;">101 Garden of Eden Road</a> | 
-  <a href="tel:+13024785660" style="text-decoration: underline;">Wilmington, DE 19803</a> | 
+  <a href="tel:+13024785660" style="text-decoration: underline;font-size:10px">Wilmington, DE 19803</a> | 
   <a href="tel:+13024785660">302-478-5660</a>
 </p>
 
@@ -176,7 +213,7 @@ Interested in getting a sneak peek at our program? Come check out the Showcase f
             </div>
 
             <div>
-                <p style="font-size: 14px; margin-top: 10px;">We like to connect in ways that work for you: <a href="#" style="color: #fff; text-decoration: none;">Update Profile</a></p>
+                <p style="font-size: 11px; margin-top: 10px;">We like to connect in ways that work for you: <a href="#" style="color: #fff; text-decoration: none;">Update Profile</a></p>
             </div>
         </div>
     </div>
